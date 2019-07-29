4a4e38f66274eeaf021f4d15ca66988d2556919b
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2019-07-29 09:33:54 -0600

    Move checkout line example in test file

--- test/disco_tests.cpp
+++ test/disco_tests.cpp
@@ -16,6 +16,45 @@ TEST(SetupTest, GoogleTestRuns)
   EXPECT_EQ(x, y);
 }
 
+TEST(AdevsUsageTest, CanRunCheckoutLineExample)
+{
+  // Expected results from ADEVS manual
+  // URL: https://web.ornl.gov/~nutarojj/adevs/adevs-docs/manual.pdf,
+  // see Tables 3.1 and 3.2
+  std::string expected_output =
+    "# Col 1: Time customer enters the line\n"
+    "# Col 2: Time required for customer checkout\n"
+    "# Col 3: Time customer leaves the store\n"
+    "# Col 4: Time spent waiting in line\n"
+    "1 1 2 0\n"
+    "2 4 6 0\n"
+    "3 4 10 3\n"
+    "5 2 12 5\n"
+    "7 10 22 5\n"
+    "8 20 42 14\n"
+    "10 2 44 32\n"
+    "11 1 45 33\n";
+  adevs::SimpleDigraph<Customer> store;
+  // Note: adevs::SimpleDigraph and adevs::Digraph take ownership of the
+  // objects used in the couple calls.
+  // As such, those objects are deleted with the destructor to the store
+  // object.
+  // Therefore, delete is not called on c, g, or o...
+  auto c = new Clerk();
+  auto g = new Generator();
+  auto o = new Observer();
+  store.couple(g, c);
+  store.couple(c, o);
+  adevs::Simulator<Customer> sim;
+  store.add(&sim);
+  while (sim.next_event_time() < adevs_inf<adevs::Time>())
+  {
+    sim.exec_next_event();
+  }
+  std::string actual_output = o->get_results();
+  EXPECT_EQ(expected_output, actual_output);
+}
+
 TEST(DiscoUtilFunctions, TestClamp)
 {
   // POSITIVE INTEGERS
@@ -124,45 +163,6 @@ TEST(DiscoBasicTest, CanRunPowerLimitedSink)
   std::string actual_sink_output = sink->get_results();
   EXPECT_EQ(expected_src_output, actual_src_output);
   EXPECT_EQ(expected_sink_output, actual_sink_output);
-}
-
-TEST(AdevsUsageTest, CanRunCheckoutLineExample)
-{
-  // Expected results from ADEVS manual
-  // URL: https://web.ornl.gov/~nutarojj/adevs/adevs-docs/manual.pdf,
-  // see Tables 3.1 and 3.2
-  std::string expected_output =
-    "# Col 1: Time customer enters the line\n"
-    "# Col 2: Time required for customer checkout\n"
-    "# Col 3: Time customer leaves the store\n"
-    "# Col 4: Time spent waiting in line\n"
-    "1 1 2 0\n"
-    "2 4 6 0\n"
-    "3 4 10 3\n"
-    "5 2 12 5\n"
-    "7 10 22 5\n"
-    "8 20 42 14\n"
-    "10 2 44 32\n"
-    "11 1 45 33\n";
-  adevs::SimpleDigraph<Customer> store;
-  // Note: adevs::SimpleDigraph and adevs::Digraph take ownership of the
-  // objects used in the couple calls.
-  // As such, those objects are deleted with the destructor to the store
-  // object.
-  // Therefore, delete is not called on c, g, or o...
-  auto c = new Clerk();
-  auto g = new Generator();
-  auto o = new Observer();
-  store.couple(g, c);
-  store.couple(c, o);
-  adevs::Simulator<Customer> sim;
-  store.add(&sim);
-  while (sim.next_event_time() < adevs_inf<adevs::Time>())
-  {
-    sim.exec_next_event();
-  }
-  std::string actual_output = o->get_results();
-  EXPECT_EQ(expected_output, actual_output);
 }
 
 int

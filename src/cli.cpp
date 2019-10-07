38944d6cfd4bde1109d246cb540da879197b5cb7
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2019-10-07 16:14:49 -0600

    Further enhance main method for CLI

--- src/cli.cpp
+++ src/cli.cpp
@@ -6,13 +6,18 @@
 
 int
 main(int argc, char *argv[]) {
-  if (argc != 3)
-    // TODO: Add detailed error message
+  if (argc != 3) {
+    std::cout << "USAGE: aren <input_file_path> <output_file_path>\n";
+    std::cout << "  - input_file_path: path to TOML input file\n";
+    std::cout << "  - output_file_path: path to TOML output file\n";
+    std::cout << "SETS Exit Code 1 if issues encountered, else sets 0\n";
     return 1;
+  }
   auto input_toml = std::string{argv[1]};
   auto output_csv = std::string{argv[2]};
 
   auto m = DISCO::Main(input_toml, output_csv);
-  std::cout << "Success!\n";
+  auto out = m.run();
+  std::cout << "result of m.run() = " << out << "\n";
   return 0;
 }

f0a7a4b0cef8e86a38771e8c77325e4cc1852bc7
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2019-11-13 09:39:08 -0700

    Update CLI to use statistics printer and take a statistics CSV file path.
    The command line interface now takes a statistics file (CSV) to write to. I've also done some renaming of variables and updated the usage description which incorrectly claimed output was in TOML format (it's CSV).

--- src/cli.cpp
+++ src/cli.cpp
@@ -7,21 +7,24 @@
 
 int
 main(int argc, char *argv[]) {
-  if (argc != 4) {
+  if (argc != 5) {
     std::cout << "USAGE: e2rin <input_file_path> <output_file_path> "
-                 "<scenario_id>\n"
-                 "  - input_file_path: path to TOML input file\n"
-                 "  - output_file_path: path to TOML output file\n"
-                 "  - scenario_id: the id of the scenario to run\n"
+                 "<stats_file_path> <scenario_id>\n"
+                 "  - input_file_path : path to TOML input file\n"
+                 "  - output_file_path: path to CSV output file for time-series data\n"
+                 "  - stats_file_path : path to CSV output file for statistics\n"
+                 "  - scenario_id     : the id of the scenario to run\n"
                  "SETS Exit Code 1 if issues encountered, else sets 0\n";
     return 1;
   }
   auto input_toml = std::string{argv[1]};
-  auto output_csv = std::string{argv[2]};
-  auto scenario_id = std::string{argv[3]};
-  DB_PUTS("input_toml  : " << input_toml);
-  DB_PUTS("output_csv  : " << output_csv);
-  DB_PUTS("scenario_id : \"" << scenario_id << "\"");
+  auto timeseries_csv = std::string{argv[2]};
+  auto stats_csv = std::string{argv[3]};
+  auto scenario_id = std::string{argv[4]};
+  std::cout << "input_toml      : " << input_toml << "\n";
+  std::cout << "timeseries_csv  : " << timeseries_csv << "\n";
+  std::cout << "stats_csv       : " << stats_csv << "\n";
+  std::cout << "scenario_id     : \"" << scenario_id << "\"" << "\n";
 
   auto m = ::ERIN::Main{input_toml};
   ::ERIN::ScenarioResults out;
@@ -36,17 +39,28 @@ main(int argc, char *argv[]) {
     return 1;
   }
   std::cout << "result of m.run(\"" << scenario_id << "\") = "
-    << out.get_is_good() << "\n";
-  if (!out.get_is_good())
+            << out.get_is_good() << "\n";
+  std::cout << "max_time = " << max_time << "\n";
+  if (!out.get_is_good()) {
     return 1;
-  std::ofstream csv{output_csv, std::ios::out | std::ios::trunc};
-  if (csv.is_open())
+  }
+  std::ofstream csv{timeseries_csv, std::ios::out | std::ios::trunc};
+  if (csv.is_open()) {
     csv << out.to_csv(max_time);
+  }
   else {
-    std::cerr << "unable to open output_csv for writing \""
-              << output_csv << "\"\n";
+    std::cerr << "unable to open timeseries_csv for writing \""
+              << timeseries_csv << "\"\n";
     return 1;
   }
   csv.close();
+  std::ofstream stats{stats_csv, std::ios::out | std::ios::trunc};
+  if (stats.is_open()) {
+    stats << out.to_stats_csv();
+  }
+  else {
+    std::cerr << "unable to open stats_csv for writing \""
+              << stats_csv << "\"\n";
+  }
   return 0;
 }

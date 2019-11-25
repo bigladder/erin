aa8073a6449f3bbae1e1c93d508e3628a09b8d42
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2019-11-25 15:09:01 -0700

    Add gsl library to source code and use it for span.

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,15 +1,17 @@
 /* Copyright (c) 2019 Big Ladder Software LLC. All rights reserved.
 * See the LICENSE file for additional terms and conditions. */
-#include <fstream>
-#include <iostream>
 #include "debug_utils.h"
 #include "erin/erin.h"
+#include "gsl/span"
+#include <fstream>
+#include <iostream>
 
 constexpr int num_args{4};
 
 int
-main(int argc, char *argv[])
+main(const int argc, const char* argv[])
 {
+  auto args = gsl::span<const char*>(argv, argc);
   if (argc != (num_args + 1)) {
     std::cout << "USAGE: e2rin <input_file_path> <output_file_path> "
                  "<stats_file_path> <scenario_id>\n"
@@ -20,10 +22,10 @@ main(int argc, char *argv[])
                  "SETS Exit Code 1 if issues encountered, else sets 0\n";
     return 1;
   }
-  std::string input_toml{argv[1]};
-  std::string timeseries_csv{argv[2]};
-  std::string stats_csv{argv[3]};
-  std::string scenario_id{argv[4]};
+  std::string input_toml{args[1]};
+  std::string timeseries_csv{args[2]};
+  std::string stats_csv{args[3]};
+  std::string scenario_id{args[4]};
   std::cout << "input_toml      : " << input_toml << "\n";
   std::cout << "timeseries_csv  : " << timeseries_csv << "\n";
   std::cout << "stats_csv       : " << stats_csv << "\n";

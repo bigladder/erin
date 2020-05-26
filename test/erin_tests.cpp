9e97ec150308822ebe26872ef52d736a09fa40fd
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2020-05-26 17:40:25 -0600

    Add basic test to calculate the reliability schedule for components.

--- test/erin_tests.cpp
+++ test/erin_tests.cpp
@@ -5676,6 +5676,124 @@ TEST(ErinBasicsTest, Test_energy_balance_on_mux_with_replay)
   ASSERT_EQ(dt4, 0);
 }
 
+TEST(ErinBasicsTest, Test_that_we_can_calculate_reliability_schedule)
+{
+  using size_type = std::vector<int>::size_type;
+  constexpr size_type DISTRIBUTION_TYPE_FIXED = 0;
+  std::vector<size_type> component_id_to_failure_mode_distribution_id{0};
+  std::vector<size_type> component_id_to_repair_mode_distribution_id{1};
+  const auto num_components = component_id_to_failure_mode_distribution_id.size();
+  std::vector<size_type> distribution_id_to_distribution_type{
+    DISTRIBUTION_TYPE_FIXED,
+    DISTRIBUTION_TYPE_FIXED,
+  };
+  std::vector<size_type> distribution_id_to_distribution_type_id{0, 1};
+  std::vector<int64_t> fixed_distribution_attr_value{5, 1};
+  std::unordered_map<size_type, std::vector<size_type>> failure_mode_types_to_component_ids{};
+  std::unordered_map<size_type, std::vector<size_type>> failure_mode_types_to_dist_ids{};
+  std::unordered_map<size_type, std::vector<size_type>> repair_types_to_component_ids{};
+  std::unordered_map<size_type, std::vector<size_type>> repair_types_to_dist_ids{};
+  std::vector<int64_t> comp_id_to_time(num_components, 0);
+  std::unordered_map<size_type, std::vector<int64_t>> comp_id_to_reliability_schedule_times{};
+  std::unordered_map<size_type, std::vector<bool>> comp_id_to_reliability_schedule_states{};
+  for (size_type component_id{0}; component_id < num_components; ++component_id) {
+    comp_id_to_reliability_schedule_times.emplace(component_id, std::vector<int64_t>{0});
+    comp_id_to_reliability_schedule_states.emplace(component_id, std::vector<bool>{true});
+    const auto& failure_mode_distribution_id = component_id_to_failure_mode_distribution_id.at(component_id);
+    const auto& repair_mode_distribution_id = component_id_to_repair_mode_distribution_id.at(component_id);
+    //if ((failure_mode_distribution_id == 0) || (repair_mode_distribution_id == 0)) {
+    //  continue;
+    //}
+    const auto& fm_dist_type_id = distribution_id_to_distribution_type.at(failure_mode_distribution_id);
+    const auto& repair_dist_type_id = distribution_id_to_distribution_type.at(repair_mode_distribution_id);
+    auto it_fm_type = failure_mode_types_to_component_ids.find(fm_dist_type_id);
+    if (it_fm_type == failure_mode_types_to_component_ids.end()) {
+      failure_mode_types_to_component_ids[fm_dist_type_id] = {component_id};
+      failure_mode_types_to_dist_ids[fm_dist_type_id] = {failure_mode_distribution_id};
+    }
+    else {
+      failure_mode_types_to_component_ids[fm_dist_type_id].emplace_back(component_id);
+      failure_mode_types_to_dist_ids[fm_dist_type_id].emplace_back(failure_mode_distribution_id);
+    }
+    auto it_rm_type = repair_types_to_component_ids.find(repair_dist_type_id);
+    if (it_rm_type == repair_types_to_component_ids.end()) {
+      repair_types_to_component_ids[repair_dist_type_id] = {component_id};
+      repair_types_to_dist_ids[repair_dist_type_id] = {repair_mode_distribution_id};
+    }
+    else {
+      repair_types_to_component_ids[repair_dist_type_id].emplace_back(component_id);
+      repair_types_to_dist_ids[repair_dist_type_id].emplace_back(repair_mode_distribution_id);
+    }
+  }
+  const int64_t final_time{10};
+  size_type count{0};
+  while (true) {
+    count = 0;
+    for (const auto& fm_comp_pair : failure_mode_types_to_component_ids) {
+      const auto& fm_type = fm_comp_pair.first;
+      for (size_type i{0}; i < fm_comp_pair.second.size(); ++i) {
+        const auto& comp_id = fm_comp_pair.second.at(i);
+        const auto& dist_id = failure_mode_types_to_dist_ids.at(fm_type).at(i);
+        const auto& time = comp_id_to_time.at(comp_id);
+        if (time >= final_time) {
+          ++count;
+          continue;
+        }
+        int64_t dt{0};
+        switch (fm_type) {
+          case DISTRIBUTION_TYPE_FIXED:
+            {
+              dt = fixed_distribution_attr_value.at(dist_id);
+              auto next_time = time + dt;
+              comp_id_to_reliability_schedule_times[comp_id].emplace_back(next_time);
+              comp_id_to_reliability_schedule_states[comp_id].emplace_back(false);
+              comp_id_to_time[comp_id] = next_time;
+            }
+          default:
+            std::cout << "shouldn't get here...\n";
+        }
+      }
+    }
+    if (count == num_components) {
+      break;
+    }
+    count = 0;
+    for (const auto& rm_comp_pair : repair_types_to_component_ids) {
+      const auto& rm_type = rm_comp_pair.first;
+      for (size_type i{0}; i < rm_comp_pair.second.size(); ++i) {
+        const auto& comp_id = rm_comp_pair.second.at(i);
+        const auto& dist_id = repair_types_to_dist_ids.at(rm_type).at(i);
+        const auto& time = comp_id_to_time.at(comp_id);
+        if (time >= final_time) {
+          ++count;
+          continue;
+        }
+        int64_t dt{0};
+        switch (rm_type) {
+          case DISTRIBUTION_TYPE_FIXED:
+            {
+              dt = fixed_distribution_attr_value.at(dist_id);
+              auto next_time = time + dt;
+              comp_id_to_reliability_schedule_times[comp_id].emplace_back(next_time);
+              comp_id_to_reliability_schedule_states[comp_id].emplace_back(true);
+              comp_id_to_time[comp_id] = next_time;
+            }
+          default:
+            std::cout << "shouldn't get here...\n";
+        }
+      }
+    }
+  }
+  //std::unordered_map<size_type, std::vector<int64_t>> comp_id_to_reliability_schedule_times{};
+  //std::unordered_map<size_type, std::vector<bool>> comp_id_to_reliability_schedule_states{};
+  EXPECT_EQ(comp_id_to_reliability_schedule_times.size(), 1);
+  std::vector<int64_t> expected_times{0, 5, 6, 11};
+  EXPECT_EQ(comp_id_to_reliability_schedule_times.at(0), expected_times);
+  std::vector<bool> expected_states{true, false, true, false};
+  EXPECT_EQ(comp_id_to_reliability_schedule_states.at(0), expected_states);
+}
+
+
 int
 main(int argc, char **argv)
 {

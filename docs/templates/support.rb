fc323da000d3e2206d3dab8481ee612d0cfe05eb
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2020-07-07 16:55:22 -0600

    Further enhance support module

--- docs/templates/support.rb
+++ docs/templates/support.rb
@@ -1,6 +1,40 @@
 require 'set'
 
-module Support
+class Support
+  def initialize(load_profile_scenario_id,
+                 load_profile_building_id,
+                 load_profile_enduse,
+                 load_profile_file)
+    @load_profile_scenario_id = load_profile_scenario_id
+    @load_profile_building_id = load_profile_building_id
+    @load_profile_enduse = load_profile_enduse
+    @load_profile_file = load_profile_file
+    _check_load_profile_data
+  end
+
+  # INTERNAL METHOD
+  def _check_load_profile_data
+    num = @load_profile_building_id.length
+    warns = []
+    if num != @load_profile_scenario_id.length
+      warns << ("load_profile_building_id.length (#{num}) != "\
+                "load_profile_scenario_id.length "\
+                "(#{@load_profile_scenario_id.length})")
+    end
+    if num != @load_profile_enduse.length
+      warns << ("load_profile_building_id.length (#{num}) != "\
+                "load_profile_enduse.length (#{@load_profile_enduse.length})")
+    end
+    if num != @load_profile_file.length
+      warns << ("building_ids.length (#{num}) != "\
+                "load_profile_file.length (#{@load_profile_file.length})")
+    end
+    warns.each do |w|
+      puts "WARNING! #{w}"
+    end
+    nil
+  end
+
   # - building_ids: (Array string), array of building ids
   # - scenario_ids: (Array string), array of scenario ids
   # - enduses: (Array string), array of enduses
@@ -10,32 +44,15 @@ module Support
   #   :loads => (Array {:load_id=>string, :file=>string}), the load data
   #   :load_ids => (Array string), the load ids used
   #   }
-  def self.process_load_data(building_ids, scenario_ids, enduses, load_profiles)
+  def process_load_data
     loads = []
     warns = []
     load_id_set = Set.new
     load_ids = []
-    num = building_ids.length
-    if num != scenario_ids.length
-      warns << ("building_ids.length (#{num}) != "\
-                "scenario_ids.length (#{scenario_ids.length})")
-    elsif num != enduses.length
-      warns << ("building_ids.length (#{num}) != "\
-                "enduses.length (#{enduses.length})")
-    elsif num != load_profiles.length
-      warns << ("building_ids.length (#{num}) != "\
-                "load_profiles.length (#{load_profiles.length})")
-    end
-    if warns.length > 0
-      return {
-        warnings: warns,
-        loads: loads,
-      }
-    end
-    building_ids.each_with_index do |b_id, n|
-      s_id = scenario_ids[n]
-      enduse = enduses[n]
-      file = load_profiles[n]
+    @load_profile_building_id.each_with_index do |b_id, n|
+      s_id = @load_profile_scenario_id[n]
+      enduse = @load_profile_enduse[n]
+      file = @load_profile_file[n]
       load_id = "#{b_id}_#{enduse}_#{s_id}"
       if load_id_set.include?(load_id)
         warns << "WARNING! Duplicate load_id \"#{load_id}\""
@@ -49,5 +66,34 @@ module Support
       loads: loads,
       load_ids: load_ids,
     }
+  end
+
+  #
+  #def create_building_config(building_ids, egen_flags, egen_eff_pcts, heat_storage_flags, heat_storage_caps, gas_boiler_flags, 
+  #  building_config = {}
+  #  building_level_building_id.each_with_index do |b_id, n|                     
+  #    if building_config.include?(b_id)                                         
+  #    end                                                                       
+  #    building_config[b_id] = {                                                 
+  #      has_egen: is_true(building_level_egen_flag[n]),                         
+  #      egen_eff: building_level_egen_eff_pct[n].to_f,                          
+  #      has_tes: is_true(building_level_heat_storage_flag[n]),                  
+  #      tes_cap_kWh: building_level_heat_storage_cap_kWh[n].to_f,               
+  #      has_boiler: is_true(building_level_gas_boiler_flag[n]),                 
+  #      boiler_eff: building_level_gas_boiler_eff_pct[n].to_f / 100.0,          
+  #      e_supply_node: building_level_electricity_supply_node[n].strip,         
+  #      ng_supply_node: "utility",                                              
+  #      enduses: Set.new,                                                       
+  #    }                                                                         
+  #end                                                                         
+
+  #
+  def create_node_config
+  end
+  #
+  def generate_connections
+  end
+  #
+  def generate_components
   end
 end

f6a37b6ec5003810637715432b1f63880bb71943
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2020-07-13 16:08:33 -0600

    Test for local electric generator + battery

--- docs/templates/test_v2.rb
+++ docs/templates/test_v2.rb
@@ -236,4 +236,72 @@ class TestTemplate < Minitest::Test
     assert_equal(data.fetch(:muxer_component, []).length, 0)
     assert_equal(expected, achieved, "non-matches: #{achieved - expected}")
   end
+
+  def test_connecting_utility_electricity_to_local_source_plus_battery_and_egen
+    data = {
+      load_component: [
+        {
+          location_id: "b1",
+          inflow: "electricity",
+        },
+      ],
+      source_component: [
+        {
+          location_id: "utility",
+          outflow: "electricity",
+          is_limited: "FALSE",
+          max_outflow_kW: 0.0,
+        },
+        {
+          location_id: "utility",
+          outflow: "natural_gas",
+          is_limited: "FALSE",
+          max_outflow_kW: 0.0,
+        },
+      ],
+      converter_component: [
+        {
+          location_id: "b1",
+          inflow: "natural_gas",
+          outflow: "electricity",
+          constant_efficiency: 0.5,
+        },
+      ],
+      storage_component: [
+        {
+          location_id: "b1",
+          flow: "electricity",
+          capacity_kWh: 100.0,
+          max_inflow_kW: 10.0,
+        },
+      ],
+      network_link: [
+        {
+          source_location_id: "utility",
+          destination_location_id: "b1",
+          flow: "electricity",
+        },
+        {
+          source_location_id: "utility",
+          destination_location_id: "b1",
+          flow: "natural_gas",
+        },
+      ],
+    }
+    Support.generate_connections(data)
+    assert_equal(data.fetch(:load_component, []).length, 1)
+    assert_equal(data.fetch(:source_component, []).length, 2)
+    assert_equal(data.fetch(:converter_component, []).length, 1)
+    assert_equal(data.fetch(:storage_component, []).length, 1)
+    assert_equal(data.fetch(:muxer_component, []).length, 1)
+    achieved = Set.new(data[:connection])
+    expected = Set.new([
+      ["utility_natural_gas_source:OUT(0)", "b1_electricity_generator:IN(0)", "natural_gas"],
+      ["b1_electricity_generator:OUT(0)", "b1_electricity_bus:IN(1)", "electricity"],
+      ["utility_electricity_source:OUT(0)", "b1_electricity_bus:IN(0)", "electricity"],
+      ["b1_electricity_bus:OUT(0)", "b1_electricity_store:IN(0)", "electricity"],
+      ["b1_electricity_store:OUT(0)", "b1_electricity:IN(0)", "electricity"],
+    ])
+    assert_equal(expected, achieved, "non-matches: #{achieved - expected}")
+  end
 end

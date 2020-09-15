4be8519706ee2fcb487f3eacd3b79f766ec3c419
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2020-09-15 11:47:59 -0600

    Wrap in try/catch to prevent exception in main.

--- src/erin_csv.cpp
+++ src/erin_csv.cpp
@@ -17,28 +17,34 @@ main(const int argc, const char* argv[])
     return 1;
   }
   std::string csv_path{args[1]};
-  std::ifstream ifs{csv_path};
-  if (!ifs.is_open()) {
-    std::ostringstream oss;
-    oss << "input file stream on \"" << csv_path
-        << "\" failed to open for reading\n";
-    std::cout << oss.str() << "\n";
+  try {
+    std::ifstream ifs{csv_path};
+    if (!ifs.is_open()) {
+      std::ostringstream oss;
+      oss << "input file stream on \"" << csv_path
+          << "\" failed to open for reading\n";
+      std::cout << oss.str() << "\n";
+      return 1;
+    }
+    std::cout << "Contents of " << csv_path << ":\n";
+    for (int row{0}; ifs.good(); ++row) {
+      std::string delim{""};
+      auto cells = csv::read_row(ifs);
+      if (cells.size() == 0) {
+        break;
+      }
+      std::cout << row << ": ";
+      for (const auto& c : cells) {
+        std::cout << delim << c;
+        delim = ",";
+      }
+      std::cout << "\n";
+    }
+    ifs.close();
+  }
+  catch (const std::exception& e) {
+    std::cout << "Unhandled exception: " << e.what() << "\n";
     return 1;
   }
-  std::cout << "Contents of " << csv_path << ":\n";
-  for (int row{0}; ifs.good(); ++row) {
-    std::string delim{""};
-    auto cells = csv::read_row(ifs);
-    if (cells.size() == 0) {
-      break;
-    }
-    std::cout << row << ": ";
-    for (const auto& c : cells) {
-      std::cout << delim << c;
-      delim = ",";
-    }
-    std::cout << "\n";
-  }
-  ifs.close();
   return 0;
 }

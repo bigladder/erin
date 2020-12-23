ddf5bbafd5ed662325023e77865842e9816cd4aa
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2020-12-23 12:22:09 -0700

    Prevent potential exceptions in main.

--- src/cli_test_cdf.cpp
+++ src/cli_test_cdf.cpp
@@ -6,17 +6,18 @@
 #include "erin_csv.h"
 #include "gsl/span"
 #include <cstdint>
+#include <exception>
 #include <fstream>
 #include <string>
 #include <iostream>
 
 void
-print_usage(const int argc, const gsl::span<const char*>& args)
+print_usage(const gsl::span<const char*>& args)
 {
   namespace ev = erin::version;
   namespace eu = erin::utils;
   auto exe_name = eu::path_to_filename(args[0]);
-  for (int idx{0}; idx < argc; ++idx) {
+  for (int idx{0}; idx < args.size(); ++idx) {
     std::cout << "arg[" << idx << "] = " << args[idx] << "\n";
   }
   std::cout << exe_name << " version " << ev::version_string << "\n";
@@ -46,14 +47,23 @@ constexpr int num_args_for_normal{5};
 constexpr int num_args_for_table{4};
 
 int
-main(const int argc, const char* argv[])
+run_it(const gsl::span<const char*>& args)
 {
+  const int argc{static_cast<int>(args.size())};
   namespace ev = erin::version;
   namespace eu = erin::utils;
   namespace ed = erin::distribution;
-  auto args = gsl::span<const char*>(argv, argc);
   auto cds = ed::CumulativeDistributionSystem{};
-  auto cdf_type = ed::tag_to_cdf_type(args[1]);
+  ed::CdfType cdf_type{ed::CdfType::Fixed};
+  try {
+    cdf_type = ed::tag_to_cdf_type(args[1]);
+  }
+  catch (const std::exception&) {
+    std::ostringstream oss{};
+    oss << "issue parsng cdf_type \"" << args[1] << "\"\n";
+    std::cerr << oss.str();
+    return 1;
+  }
   const std::string tag{"cdf"};
   ed::size_type id{};
   ed::size_type num_samples{};
@@ -62,49 +72,138 @@ main(const int argc, const char* argv[])
       {
         if (argc != num_args_for_fixed) {
           std::cout << "Missing arguments for fixed distribution\n";
-          print_usage(argc, args);
+          print_usage(args);
           return 1;
         }
-        std::int64_t value_in_seconds = std::stol(args[2]);
-        num_samples = static_cast<ed::size_type>(std::stol(args[3]));
-        id = cds.add_fixed_cdf(tag, value_in_seconds);
+        std::int64_t value_in_seconds{0};
+        try {
+          value_in_seconds = std::stol(args[2]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Value in seconds must be convertable to an int64\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          num_samples = static_cast<ed::size_type>(std::stol(args[3]));
+        }
+        catch (const std::exception&) {
+          std::cout << "Number of samples must be convertable to an int64\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          id = cds.add_fixed_cdf(tag, value_in_seconds);
+        }
+        catch (const std::exception&) {
+          std::cout << "Error creating a fixed distribution\n";
+          print_usage(args);
+          return 1;
+        }
         break;
       }
     case ed::CdfType::Uniform:
       {
         if (argc != num_args_for_uniform) {
           std::cout << "Missing arguments for uniform distribution\n";
-          print_usage(argc, args);
+          print_usage(args);
           return 1;
         }
-        std::int64_t lower_bound = std::stol(args[2]);
-        std::int64_t upper_bound = std::stol(args[3]);
-        num_samples = static_cast<ed::size_type>(std::stol(args[4]));
-        id = cds.add_uniform_cdf(tag, lower_bound, upper_bound);
+        std::int64_t lower_bound{0};
+        std::int64_t upper_bound{1};
+        try {
+          lower_bound = std::stol(args[2]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Lower bound must be convertable to int64 for uniform distribution\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          upper_bound = std::stol(args[3]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Upper bound must be convertable to int64 for uniform distribution\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          num_samples = static_cast<ed::size_type>(std::stol(args[4]));
+        }
+        catch (const std::exception&) {
+          std::cout << "Number of samples must be convertable to an int64\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          id = cds.add_uniform_cdf(tag, lower_bound, upper_bound);
+        }
+        catch (const std::exception&) {
+          std::cout << "Error creating a uniform distribution\n";
+          print_usage(args);
+          return 1;
+        }
         break;
       }
     case ed::CdfType::Normal:
       {
         if (argc != num_args_for_normal) {
-          std::cout << "Missing arguments for uniform distribution\n";
-          print_usage(argc, args);
+          std::cout << "Missing arguments for normal distribution\n";
+          print_usage(args);
           return 1;
         }
-        std::int64_t mean = std::stol(args[2]);
-        std::int64_t stdev = std::stol(args[3]);
-        num_samples = static_cast<ed::size_type>(std::stol(args[4]));
-        id = cds.add_normal_cdf(tag, mean, stdev);
+        std::int64_t mean{0};
+        std::int64_t stdev{0};
+        try {
+          mean = std::stol(args[2]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Mean must be convertable to an int64 for normal\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          stdev = std::stol(args[3]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Standard deviation must be convertable to an int64 for normal\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          num_samples = static_cast<ed::size_type>(std::stol(args[4]));
+        }
+        catch (const std::exception&) {
+          std::cout << "Number of samples must be convertable to an int64\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          id = cds.add_normal_cdf(tag, mean, stdev);
+        }
+        catch (const std::exception&) {
+          std::cout << "Error creating a normal distribution\n";
+          print_usage(args);
+          return 1;
+        }
         break;
       }
     case ed::CdfType::Table:
       {
         if (argc != num_args_for_table) {
           std::cout << "Missing arguments for table distribution\n";
-          print_usage(argc, args);
+          print_usage(args);
           return 1;
         }
         std::string csv_path{args[2]};
-        num_samples = static_cast<ed::size_type>(std::stol(args[3]));
+        try {
+          num_samples = static_cast<ed::size_type>(std::stol(args[4]));
+        }
+        catch (const std::exception&) {
+          std::cout << "Number of samples must be convertable to an int64\n";
+          print_usage(args);
+          return 1;
+        }
         std::vector<double> xs{};
         std::vector<double> dtimes_s{};
         std::ifstream ifs{csv_path};
@@ -130,23 +229,64 @@ main(const int argc, const char* argv[])
             std::cerr << oss.str() << "\n";
             return 1;
           }
-          xs.emplace_back(std::stod(cells[0]));
-          dtimes_s.emplace_back(std::stod(cells[1]));
+          try {
+            xs.emplace_back(std::stod(cells[0]));
+            dtimes_s.emplace_back(std::stod(cells[1]));
+          }
+          catch (const std::exception&) {
+            std::ostringstream oss{};
+            oss << "issue reading input file csv \"" << csv_path
+                << "\"; issue on row " << row
+                << "; could not conver xs (" << cells[0]
+                << ") or dtimes (" << cells[1] << " to double\n";
+            std::cerr << oss.str() << "\n";
+            return 1;
+          }
         }
         ifs.close();
-        id = cds.add_table_cdf(tag, xs, dtimes_s);
+        try {
+          id = cds.add_table_cdf(tag, xs, dtimes_s);
+        }
+        catch (const std::exception&) {
+          std::cout << "Error creating a tabular distribution\n";
+          print_usage(args);
+          return 1;
+        }
         break;
       }
     default:
       {
-        print_usage(argc, args);
+        print_usage(args);
         return 1;
       }
   }
   using size_type = std::vector<std::int64_t>::size_type;
   std::cout << "data\n";
   for (size_type idx{0}; idx < num_samples; ++idx) {
-    std::cout << cds.next_time_advance(id) << "\n";
+    try {
+      std::cout << cds.next_time_advance(id) << "\n";
+    }
+    catch (const std::exception&) {
+      std::cout << "Error attempting to sample distribution on sample "
+                << idx << "\n";
+      print_usage(args);
+      return 1;
+    }
   }
   return 0;
+}
+
+int
+main(const int argc, const char* argv[])
+{
+  int ret_val{0};
+  auto args = gsl::span<const char*>(argv, argc);
+  try {
+    ret_val = run_it(args);
+  }
+  catch (const std::exception&) {
+    std::cerr << "an unhandled exception occurred...\n";
+    ret_val = 1;
+  }
+  return ret_val;
 }

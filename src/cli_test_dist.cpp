1f09eef4e3e0e32c36130c00e2f7289b56ccbf81
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2021-02-03 16:06:27 -0700

    Update distribution test
    Enable distribution test to exercise the Weibull.
    Prevent crashing if the wrong number of args are given.

--- src/cli_test_dist.cpp
+++ src/cli_test_dist.cpp
@@ -5,38 +5,51 @@
 #include "erin/version.h"
 #include "erin_csv.h"
 #include "gsl/span"
+#include <algorithm>
 #include <cstdint>
 #include <exception>
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <vector>
 
 void
 print_usage(const gsl::span<const char*>& args)
 {
   namespace ev = erin::version;
   namespace eu = erin::utils;
-  auto exe_name = eu::path_to_filename(args[0]);
+  auto exe_name = (args.size() > 0)
+    ? eu::path_to_filename(args[0])
+    : std::string{""};
   for (int idx{0}; idx < args.size(); ++idx) {
     std::cout << "arg[" << idx << "] = " << args[idx] << "\n";
   }
   std::cout << exe_name << " version " << ev::version_string << "\n";
   std::cout << "USAGE: " << exe_name << " <distribution_name> <dist_param_1> <dist_param_2> <dist_param_3>\n"
-               "  - distribution_name: one of 'fixed', 'uniform', 'normal', 'table'\n"
+               "  - distribution_name: one of 'fixed', 'uniform', 'normal', 'weibull', 'table'\n"
                "  - dist_param_1     :\n"
                "    - fixed          => the fixed value\n"
                "    - uniform        => the lower bound\n"
                "    - normal         => the mean of the distribution\n"
+               "    - weibull        => the shape parameter of the Weibull distribution\n"
                "    - table          => name of a CSV file with the CDF defined in two columns variate and dtime (no header)\n"
                "  - dist_param_2     :\n"
                "    - fixed          => the number of samples\n"
                "    - uniform        => the upper bound\n"
                "    - normal         => the standard deviation of the distribution\n"
+               "    - weibull        => the scale parameter of the Weibull distribution\n"
                "    - table          => the number of samples\n"
                "  - dist_param_3     :\n"
                "    - fixed          => unused\n"
                "    - uniform        => the number of samples\n"
                "    - normal         => the number of samples\n"
+               "    - weibull        => the location parameter of the Weibull distribution\n"
+               "    - table          => unused\n"
+               "  - dist_param_4     :\n"
+               "    - fixed          => unused\n"
+               "    - uniform        => unused\n"
+               "    - normal         => unused\n"
+               "    - weibull        => the number of samples\n"
                "    - table          => unused\n"
                "SETS Exit Code 1 if issues encountered, else sets 0\n";
 }
@@ -44,23 +57,37 @@ print_usage(const gsl::span<const char*>& args)
 constexpr int num_args_for_fixed{4};
 constexpr int num_args_for_uniform{5};
 constexpr int num_args_for_normal{5};
+constexpr int num_args_for_weibull{6};
 constexpr int num_args_for_table{4};
+const auto num_args_vec = std::vector<int>{
+  num_args_for_fixed,
+  num_args_for_uniform,
+  num_args_for_normal,
+  num_args_for_weibull,
+  num_args_for_table
+};
+const auto min_num_args = *std::min_element(num_args_vec.begin(), num_args_vec.end());
+const auto max_num_args = *std::max_element(num_args_vec.begin(), num_args_vec.end());
 
 int
 run_it(const gsl::span<const char*>& args)
 {
   const int argc{static_cast<int>(args.size())};
+  if ((argc < min_num_args) || (argc > max_num_args)) {
+    print_usage(args);
+    return 1;
+  }
   namespace ev = erin::version;
   namespace eu = erin::utils;
   namespace ed = erin::distribution;
-  auto cds = ed::DistributionSystem{};
+  auto ds = ed::DistributionSystem{};
   ed::DistType dist_type{ed::DistType::Fixed};
   try {
     dist_type = ed::tag_to_dist_type(args[1]);
   }
   catch (const std::exception&) {
     std::ostringstream oss{};
-    oss << "issue parsng dist_type \"" << args[1] << "\"\n";
+    oss << "issue parsing dist_type \"" << args[1] << "\"\n";
     std::cerr << oss.str();
     return 1;
   }
@@ -93,7 +120,7 @@ run_it(const gsl::span<const char*>& args)
           return 1;
         }
         try {
-          id = cds.add_fixed(tag, value_in_seconds);
+          id = ds.add_fixed(tag, value_in_seconds);
         }
         catch (const std::exception&) {
           std::cout << "Error creating a fixed distribution\n";
@@ -136,7 +163,7 @@ run_it(const gsl::span<const char*>& args)
           return 1;
         }
         try {
-          id = cds.add_uniform(tag, lower_bound, upper_bound);
+          id = ds.add_uniform(tag, lower_bound, upper_bound);
         }
         catch (const std::exception&) {
           std::cout << "Error creating a uniform distribution\n";
@@ -179,10 +206,62 @@ run_it(const gsl::span<const char*>& args)
           return 1;
         }
         try {
-          id = cds.add_normal(tag, mean, stdev);
+          id = ds.add_normal(tag, mean, stdev);
         }
         catch (const std::exception&) {
           std::cout << "Error creating a normal distribution\n";
+          print_usage(args);
+          return 1;
+        }
+        break;
+      }
+    case ed::DistType::Weibull:
+      {
+        if (argc != num_args_for_weibull) {
+          std::cout << "Missing arguments for Weibull distribution\n";
+          print_usage(args);
+          return 1;
+        }
+        double shape{0};
+        double scale{0};
+        double location{0};
+        try {
+          shape = std::stod(args[2]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Shape must be convertable to a double for Weibull\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          scale = std::stod(args[3]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Scale must be convertable to a double for Weibull\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          location = std::stod(args[4]);
+        }
+        catch (const std::exception&) {
+          std::cout << "Location must be convertable to a double for Weibull\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          num_samples = static_cast<ed::size_type>(std::stol(args[5]));
+        }
+        catch (const std::exception&) {
+          std::cout << "Number of samples must be convertable to an int64\n";
+          print_usage(args);
+          return 1;
+        }
+        try {
+          id = ds.add_weibull(tag, shape, scale, location);
+        }
+        catch (const std::exception&) {
+          std::cout << "Error creating a Weibull distribution\n";
           print_usage(args);
           return 1;
         }
@@ -245,7 +324,7 @@ run_it(const gsl::span<const char*>& args)
         }
         ifs.close();
         try {
-          id = cds.add_quantile_table(tag, xs, dtimes_s);
+          id = ds.add_quantile_table(tag, xs, dtimes_s);
         }
         catch (const std::exception&) {
           std::cout << "Error creating a tabular distribution\n";
@@ -264,7 +343,7 @@ run_it(const gsl::span<const char*>& args)
   std::cout << "data\n";
   for (size_type idx{0}; idx < num_samples; ++idx) {
     try {
-      std::cout << cds.next_time_advance(id) << "\n";
+      std::cout << ds.next_time_advance(id) << "\n";
     }
     catch (const std::exception&) {
       std::cout << "Error attempting to sample distribution on sample "

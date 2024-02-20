f62b74786900f26b8609cf7053e570316ce0aacd
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-02-20 12:06:44 -0700

    Set up simple regression tests using Python

---
+++ docs/erin_next_examples/regress.py
@@ -0,0 +1,67 @@
+import sys
+import subprocess
+from pathlib import Path
+
+
+BIN_DIR = Path('.') / '..' / '..' / 'out' / 'build' / 'x64-Debug' / 'bin'
+TEST_EXE = BIN_DIR / 'test_erin_next.exe'
+CLI_EXE = BIN_DIR / 'erin_next_cli.exe'
+
+if not TEST_EXE.exists():
+    print("Cannot find test executable...")
+    sys.exit(1)
+
+
+if not CLI_EXE.exists():
+    print("Cannot find command-line interface executable...")
+    sys.exit(1)
+
+
+def run_tests():
+    """
+    Run the test suite
+    """
+    result = subprocess.run([TEST_EXE], capture_output=True)
+    if result.returncode != 0:
+        print("Tests did not pass!")
+        print(f"stdout:\n{result.stdout}")
+        print(f"stderr:\n{result.stderr}")
+        sys.exit(1)
+
+
+def run_cli(example_name):
+    """
+    Run the CLI for example name and check output diffs
+    - example_name: string, "01" or "25" to call ex01.toml or ex25.toml
+    """
+    toml_input = f"ex{example_name}.toml"
+    result = subprocess.run([CLI_EXE, toml_input], capture_output=True)
+    if result.returncode != 0:
+        print(f"Error running CLI for example {example_name}")
+        print(f"stdout:\n{result.stdout}")
+        print(f"stderr:\n{result.stderr}")
+        sys.exit(1)
+    result = subprocess.run(
+        ['diff', 'out.csv', f'ex{example_name}-out.csv'],
+        capture_output=True)
+    if result.returncode != 0:
+        print(f"diff did not compare clean for out.csv for {example_name}")
+        print(f"stdout:\n{result.stdout}")
+        print(f"stderr:\n{result.stderr}")
+        sys.exit(1)
+    result = subprocess.run(
+        ['diff', 'stats.csv', f'ex{example_name}-stats.csv'],
+        capture_output=True)
+    if result.returncode != 0:
+        print(f'diff did not compare clean for stats.csv for {example_name}')
+        print(f"stdout:\n{result.stdout}")
+        print(f"stderr:\n{result.stderr}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    run_tests()
+    run_cli("01")
+    run_cli("02")
+    run_cli("03")
+    print("Passed all regression tests!")

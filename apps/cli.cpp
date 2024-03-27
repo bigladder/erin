d6450cc1ded474fcf80405db05214122547fdbcc
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-03-27 13:14:19 -0600

    Format apps directory; update .github actions

--- apps/cli.cpp
+++ apps/cli.cpp
@@ -17,51 +17,49 @@
 void
 PrintUsage(std::string const& progName)
 {
-	std::cout << "USAGE: " << progName << " "
-		<< "<toml-input-file> "
-		<< "<optional:output csv; default:out.csv> "
-		<< "<optional:statistics.csv; default:stats.csv> "
-		<< "<optional:scenario; default: run all>"
-		<< std::endl;
+    std::cout << "USAGE: " << progName << " " << "<toml-input-file> "
+              << "<optional:output csv; default:out.csv> "
+              << "<optional:statistics.csv; default:stats.csv> "
+              << "<optional:scenario; default: run all>" << std::endl;
 }
 
 int
 main(int argc, char** argv)
 {
-	using namespace erin;
-	std::cout << "ERIN version " << erin::version::version_string << std::endl;
-	std::cout
-		<< "Copyright (C) 2020-2024 Big Ladder Software LLC." << std::endl;
-	std::cout << "See LICENSE.txt file for license information." << std::endl;
-	if (argc >= 2)
-	{
-		std::string fname{ argv[1] };
-		std::cout << "input file: " << fname << std::endl;
-		std::ifstream ifs(fname, std::ios_base::binary);
-		if (!ifs.good())
-		{
-			std::cout << "Could not open input file stream on input file"
-				<< std::endl;
-			return EXIT_FAILURE;
-		}
-		auto nameOnly = std::filesystem::path(fname).filename();
-		auto data = toml::parse(ifs, nameOnly.string());
-		ifs.close();
-		std::cout << data << std::endl;
-		auto validationInfo = SetupGlobalValidationInfo();
-		auto maybeSim = Simulation_ReadFromToml(data, validationInfo);
-		if (!maybeSim.has_value())
-		{
-			return EXIT_FAILURE;
-		}
-		Simulation s = std::move(maybeSim.value());
-		Simulation_Print(s);
-		std::cout << "-----------------" << std::endl;
-		Simulation_Run(s);
-	}
-	else
-	{
-		PrintUsage(std::string{ argv[0] });
-	}
-	return EXIT_SUCCESS;
+    using namespace erin;
+    std::cout << "ERIN version " << erin::version::version_string << std::endl;
+    std::cout << "Copyright (C) 2020-2024 Big Ladder Software LLC."
+              << std::endl;
+    std::cout << "See LICENSE.txt file for license information." << std::endl;
+    if (argc >= 2)
+    {
+        std::string fname{argv[1]};
+        std::cout << "input file: " << fname << std::endl;
+        std::ifstream ifs(fname, std::ios_base::binary);
+        if (!ifs.good())
+        {
+            std::cout << "Could not open input file stream on input file"
+                      << std::endl;
+            return EXIT_FAILURE;
+        }
+        auto nameOnly = std::filesystem::path(fname).filename();
+        auto data = toml::parse(ifs, nameOnly.string());
+        ifs.close();
+        std::cout << data << std::endl;
+        auto validationInfo = SetupGlobalValidationInfo();
+        auto maybeSim = Simulation_ReadFromToml(data, validationInfo);
+        if (!maybeSim.has_value())
+        {
+            return EXIT_FAILURE;
+        }
+        Simulation s = std::move(maybeSim.value());
+        Simulation_Print(s);
+        std::cout << "-----------------" << std::endl;
+        Simulation_Run(s);
+    }
+    else
+    {
+        PrintUsage(std::string{argv[0]});
+    }
+    return EXIT_SUCCESS;
 }
--- apps/erin_next_stress_test.cpp
+++ apps/erin_next_stress_test.cpp
@@ -9,50 +9,51 @@ using namespace erin;
 int
 main(int argc, char** argv)
 {
-	size_t numComponents = 5000;
-	size_t numHours = 8760;
-	if (argc == 3)
-	{
-		numComponents = static_cast<size_t>(std::stoll(std::string{ argv[1] }));
-		numHours = static_cast<size_t>(std::stoll(std::string{ argv[2] }));
-	}
-	std::cout << "Running " << numComponents << " components for "
-		<< numHours << " hours" << std::endl;
-	auto start = std::chrono::high_resolution_clock::now();
-	Model m = {};
-	m.RandFn = []() { return 0.4; };
-	m.FinalTime = 8760.0 * 3600.0;
-	std::vector<TimeAndAmount> timesAndLoads = {};
-	timesAndLoads.reserve(numHours + 1);
-	for (size_t i = 0; i <= numHours; ++i)
-	{
-		timesAndLoads.push_back(TimeAndAmount{((double)i) * 3600.0, 1});
-	}
-	for (size_t i = 0; i < numComponents; ++i)
-	{
-		auto srcId = Model_AddConstantSource(m, 100);
-		auto loadId = Model_AddScheduleBasedLoad(m, timesAndLoads);
-		auto srcToLoadConn = Model_AddConnection(m, srcId, 0, loadId, 0);
-	}
-	auto stopConstr = std::chrono::high_resolution_clock::now();
-	auto durationConstr =
-		std::chrono::duration_cast<std::chrono::microseconds>(
-			stopConstr - start);
-	std::cout << "Construction time: "
-		<< ((double)durationConstr.count() / 1000.0)
-		<< " ms" << std::endl;
-	auto results = Simulate(m, false);
-	assert(results.size() == numHours + 1
-		&& "Results is not of expected length");
-	auto stop = std::chrono::high_resolution_clock::now();
-	auto durationSim =
-		std::chrono::duration_cast<std::chrono::microseconds>(
-			stop - stopConstr);
-	std::cout << "Sim time: " << ((double)durationSim.count() / 1000.0)
-		<< " ms" << std::endl;
-	auto duration =
-		std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-	std::cout << "Total time: " << ((double)duration.count() / 1000.0)
-		<< " ms" << std::endl;
-	return EXIT_SUCCESS;
+    size_t numComponents = 5'000;
+    size_t numHours = 8'760;
+    if (argc == 3)
+    {
+        numComponents = static_cast<size_t>(std::stoll(std::string{argv[1]}));
+        numHours = static_cast<size_t>(std::stoll(std::string{argv[2]}));
+    }
+    std::cout << "Running " << numComponents << " components for " << numHours
+              << " hours" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    Model m = {};
+    m.RandFn = []() { return 0.4; };
+    m.FinalTime = 8760.0 * 3600.0;
+    std::vector<TimeAndAmount> timesAndLoads = {};
+    timesAndLoads.reserve(numHours + 1);
+    for (size_t i = 0; i <= numHours; ++i)
+    {
+        timesAndLoads.push_back(TimeAndAmount{((double)i) * 3600.0, 1});
+    }
+    for (size_t i = 0; i < numComponents; ++i)
+    {
+        auto srcId = Model_AddConstantSource(m, 100);
+        auto loadId = Model_AddScheduleBasedLoad(m, timesAndLoads);
+        auto srcToLoadConn = Model_AddConnection(m, srcId, 0, loadId, 0);
+    }
+    auto stopConstr = std::chrono::high_resolution_clock::now();
+    auto durationConstr = std::chrono::duration_cast<std::chrono::microseconds>(
+        stopConstr - start
+    );
+    std::cout << "Construction time: "
+              << ((double)durationConstr.count() / 1000.0) << " ms"
+              << std::endl;
+    auto results = Simulate(m, false);
+    assert(
+        results.size() == numHours + 1 && "Results is not of expected length"
+    );
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto durationSim = std::chrono::duration_cast<std::chrono::microseconds>(
+        stop - stopConstr
+    );
+    std::cout << "Sim time: " << ((double)durationSim.count() / 1000.0) << " ms"
+              << std::endl;
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Total time: " << ((double)duration.count() / 1000.0) << " ms"
+              << std::endl;
+    return EXIT_SUCCESS;
 }

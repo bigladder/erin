cd173fb3fd0caa3ed869c70efe0c23e7217a372d
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-08-09 13:46:54 -0600

    Add new switch component and test
    This merely adds the test to get the switch state from the SimulationState but also declares the Switch structure.

---
+++ test/erin_switch_tests.cpp
@@ -0,0 +1,17 @@
+/* Copyright (c) 2024 Big Ladder Software LLC. All rights reserved.
+ * See the LICENSE.txt file for additional terms and conditions. */
+#include "erin/switch.h"
+#include "erin_next/erin_next.h"
+#include <gtest/gtest.h>
+
+using namespace ::erin;
+
+TEST(Switch, TestGetSwitchState)
+{
+    SimulationState ss = {};
+    ss.SwitchStates.push_back(SwitchState::Primary);
+    CompId id = {0, 0};
+    SwitchState actualSwitchState = SimulationState_GetSwitchState(ss, id);
+    SwitchState expectedSwitchState = SwitchState::Primary;
+    EXPECT_EQ(expectedSwitchState, actualSwitchState);
+}

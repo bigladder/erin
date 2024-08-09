21fff864874a5a085cf67be3c209a8eeebf9088d
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-08-09 13:45:29 -0600

    Add missing copyright
    The copyright header was missing on this file.

--- test/erin_lookup_table_tests.cpp
+++ test/erin_lookup_table_tests.cpp
@@ -1,3 +1,5 @@
+/* Copyright (c) 2024 Big Ladder Software LLC. All rights reserved.
+ * See the LICENSE.txt file for additional terms and conditions. */
 #include "erin_next/erin_next_lookup_table.h"
 #include <gtest/gtest.h>
 #include <vector>

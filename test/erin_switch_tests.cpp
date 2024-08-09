ef8cf29e8aeac688c31fbc7d8995f2893b6ec642
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-08-09 15:35:04 -0600

    Add Model_AddSwitch and test
    This commit consolidates all of the changes back into erin_next.h and erin_next.cpp. We will have to tackle cleaning up the physical structure in a future PR as it is too much to tackle here.

--- test/erin_switch_tests.cpp
+++ test/erin_switch_tests.cpp
@@ -1,6 +1,5 @@
 /* Copyright (c) 2024 Big Ladder Software LLC. All rights reserved.
  * See the LICENSE.txt file for additional terms and conditions. */
-#include "erin/switch.h"
 #include "erin_next/erin_next.h"
 #include <gtest/gtest.h>
 
@@ -25,4 +24,10 @@ TEST(Switch, TestSetSwitchState)
     SwitchState actualSwitchState = SimulationState_GetSwitchState(ss, id);
     SwitchState expectedSwitchState = SwitchState::Secondary;
     EXPECT_EQ(expectedSwitchState, actualSwitchState);
+}
+
+TEST(Switch, TestModelIntegration)
+{
+    Model m = {};
+    auto switchId = Model_AddSwitch(m, 0, "ATS");
 }

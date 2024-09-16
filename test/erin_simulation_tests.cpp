5b271c62d8b370ee193231095ba15c2b954abf20
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-09-16 09:08:24 -0600

    Update tests to add a new stub for testing erin_next_simulation.h

---
+++ test/erin_simulation_tests.cpp
@@ -0,0 +1,11 @@
+/* Copyright (c) 2024 Big Ladder Software LLC. All rights reserved.
+ * See the LICENSE.txt file for additional terms and conditions. */
+#include "erin_next/erin_next_simulation.h"
+#include <gtest/gtest.h>
+
+using namespace erin;
+
+TEST(ErinSim, TestReliabilityAndFragilityCurveGeneration)
+{
+  EXPECT_EQ(1, 1);
+}

9f1a4bedd073d853b14cb1251da0369ecfe4c1c9
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-09-17 11:50:06 -0600

    Refactor test to pull out common part

--- test/erin_simulation_tests.cpp
+++ test/erin_simulation_tests.cpp
@@ -1,10 +1,13 @@
 /* Copyright (c) 2024 Big Ladder Software LLC. All rights reserved.
  * See the LICENSE.txt file for additional terms and conditions. */
+#include "erin_next/erin_next.h"
 #include "erin_next/erin_next_distribution.h"
 #include "erin_next/erin_next_reliability.h"
 #include "erin_next/erin_next_simulation.h"
 #include "erin_next/erin_next_timestate.h"
+#include "gtest/gtest.h"
 #include <gtest/gtest.h>
+#include <optional>
 #include <unordered_map>
 
 using namespace erin;
@@ -99,5 +102,84 @@ TEST(ErinSim, TestCreateFailureSchedulesWithInitialAgeAndOffset)
         EXPECT_EQ(tss[0].state, false) << tss[0];
         EXPECT_EQ(tss[27].time, 168.0) << tss[27];
         EXPECT_EQ(tss[27].state, true) << tss[27];
+    }
+}
+
+std::vector<ScheduleBasedReliability>
+RunApplyReliabilitiesAndFragilities(
+    double scenarioOffset_s,
+    double scenarioDuration_s
+)
+{
+    // NOTE: our network is one source feeding electricity
+    // into one load. That is, S -> L
+    std::function<double()> randFn = []{ return 0.5; };
+    std::vector<size_t> componentFailureModeComponentIds{};
+    std::vector<double> componentInitialAges_s = {0.0, 0.0};
+    std::vector<std::string> componentTags = {"S", "L"};
+    std::vector<size_t> componentFragilityComponentIds = {0};
+    std::vector<size_t> componentFragilityFragilityModeIds = {0};
+    std::vector<size_t> fragilityModeFragilityCurveId = {0};
+    std::vector<std::optional<size_t>> fragilityModeRepairDistIds = {{}};
+    std::vector<std::string> fragilityModeTags = {"vulnerable_to_wind"};
+    std::vector<size_t> fragilityCurveCurveIds = {0};
+    std::vector<FragilityCurveType> fragilityCurveCurveTypes = {FragilityCurveType::Linear};
+    std::vector<LinearFragilityCurve> linearFragilityCurves = {
+        {.VulnerabilityId=0, .LowerBound=80.0, .UpperBound=140.0}
+    };
+    std::vector<TabularFragilityCurve> tabularFragilityCurves = {};
+    DistributionSystem ds{};
+    std::unordered_map<size_t, double> intensityIdToAmount = {
+        { 0, 160.0 },
+    };
+    std::unordered_map<size_t, std::vector<TimeState>> relSchByCompId{};
+    bool verbose = false;
+
+    return ApplyReliabilitiesAndFragilities(
+            randFn,
+            componentFailureModeComponentIds,
+            componentInitialAges_s,
+            componentTags,
+            componentFragilityComponentIds,
+            componentFragilityFragilityModeIds,
+            fragilityModeFragilityCurveId,
+            fragilityModeRepairDistIds,
+            fragilityModeTags,
+            fragilityCurveCurveIds,
+            fragilityCurveCurveTypes,
+            linearFragilityCurves,
+            tabularFragilityCurves,
+            ds,
+            scenarioOffset_s,
+            scenarioOffset_s + scenarioDuration_s,
+            intensityIdToAmount,
+            relSchByCompId,
+            verbose
+        );
+}
+
+TEST(ErinSim, TestFragility_NoReliability_NoRepair_NoOffset_NoAge)
+{
+    double scenarioOffset_s = 0.0;
+    double scenarioDuration_s = 1'000.0;
+    //std::unordered_map<size_t, double> intensityIdToAmount = {
+    //    { 0, 160.0 },
+    //};
+    //std::unordered_map<size_t, std::vector<TimeState>> relSchByCompId{};
+    std::vector<ScheduleBasedReliability> actual =
+        RunApplyReliabilitiesAndFragilities(scenarioOffset_s, scenarioDuration_s);    
+    EXPECT_EQ(actual.size(), 1);
+    for (ScheduleBasedReliability const& sbr : actual)
+    {
+        EXPECT_EQ(sbr.ComponentId, 0);
+        EXPECT_EQ(sbr.TimeStates.size(), 1);
+        EXPECT_EQ(sbr.TimeStates[0].time, 0.0);
+        EXPECT_EQ(sbr.TimeStates[0].state, false);
+        EXPECT_EQ(sbr.TimeStates[0].failureModeCauses.size(), 0);
+        EXPECT_EQ(sbr.TimeStates[0].fragilityModeCauses.size(), 1);
+        for (size_t fmId : sbr.TimeStates[0].fragilityModeCauses)
+        {
+            EXPECT_EQ(fmId, 0);
+        }
     }
 }

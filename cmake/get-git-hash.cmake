b0225eff0d32377709fd6542e20d36339220e1d0
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-10-31 17:17:37 -0600

    Update to enable simple git hash determination to work

--- cmake/get-git-hash.cmake
+++ cmake/get-git-hash.cmake
@@ -4,7 +4,6 @@
 execute_process(
     COMMAND git log -1 --format=%h
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE ERIN_GIT_HASH
+    OUTPUT_VARIABLE PROJECT_GIT_HASH
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-configure_file(${CMAKE_SOURCE_DIR}/include/erin/version.h.in ${CMAKE_SOURCE_DIR}/include/erin/version.h @ONLY)

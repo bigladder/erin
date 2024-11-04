095829ce9daa273fea51431bdcd201e53a251ead
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-11-04 11:39:04 -0700

    Reorganize header.

--- include/erin/graph.h
+++ include/erin/graph.h
@@ -2,17 +2,21 @@
 // See the LICENSE.txt file for additional terms and conditions.
 #ifndef ERIN_GRAPH_H
 #define ERIN_GRAPH_H
-#include "erin.h"
+
 #include <string>
 #include <vector>
 
+#include "erin.h"
+
 namespace erin
 {
+
 // PUBLIC
 std::string network_to_dot(std::vector<Connection> const& connections,
                            std::vector<std::string> const& component_tag_by_id,
                            std::string const& graph_name,
                            bool use_html_label = true);
+
 } // namespace erin
 
 #endif

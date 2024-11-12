44a0628f65bc395291f0e0feec566a9dce33839d
Michael O'Keefe <michael.okeefe@bigladdersoftware.com>
2024-11-12 10:37:28 -0700

    Fix issue with timestep not interacting with finalTime correctly

--- src/erin_timestate.cpp
+++ src/erin_timestate.cpp
@@ -367,7 +367,7 @@ void TimeState_CountAndTimeFailureEvents(std::vector<TimeState> const& tss,
             }
         }
         TimeState const nextTs =
-            (i + 1) < tss.size() ? tss[i + 1] : TimeState {finalTime_s, ts.state, {}, {}};
+            (i + 1) < tss.size() && tss[i + 1].time <= finalTime_s ? tss[i + 1] : TimeState {finalTime_s, ts.state, {}, {}};
         double dt = nextTs.time - ts.time;
         if (dt <= 0.0)
         {
